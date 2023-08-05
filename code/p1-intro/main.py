edb6a436447cac0f8826bbf62973456473fca9cf
Helder Pereira <phelder@gmail.com>
2023-08-05 08:27:53 +0200

    Lesson 1 code

---
+++ code/p1-intro/main.py
@@ -0,0 +1,41 @@
+print("Hello World")
+print("How are you doing?")
+print("Whatever...")
+
+# = is the attribution operator
+# it is used to put values inside of variables
+
+myName = "Helder"
+
+print(myName)
+
+
+# Math Operators
+# +      -     *      /      %
+
+number1 = 5
+number2 = 2
+
+sum = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+remainder = number1 % number2
+
+print(sum)
+print(subtraction)
+print(multiplication)
+print(division)
+print(remainder)
+
+# Concatenation or String Join
+# Hello my name is Helder and I am 42 years old!
+
+myName = "Helder"
+myAge = 42
+
+greeting = "Hello my name is " + myName
+greeting2 = "My age is " + str(myAge)
+
+print(greeting)
+print(greeting2)
---
+++ code/p2-conditions/main.py
@@ -0,0 +1,29 @@
+# Operators we have learned so far
+
+#  =   for attributing a value to a variable
+#  +  -  *  /   %    for Math operations
+#  +  can also be used for concatenation
+
+# CONDITIONAL OPERATORS
+#    ==     VALUES ARE THE SAME
+#    !=     VALUES ARE DIFFERENT
+#    >      IS BIGGER
+#    <      IS SMALLER
+#    >=     IS BIGGER OR IS EQUAL
+#    <=     IS SMALLER OR IS EQUAL
+
+#   the result of a conditional operation is a Boolean / Bool
+
+age = 18
+
+if (age >= 18):
+    print("Hi, have a beer!")
+else:
+    print("Hi, have a fritz-cola!")
+
+print("Bye Bye!")
+
+number1 = 54
+number2 = 34
+
+
